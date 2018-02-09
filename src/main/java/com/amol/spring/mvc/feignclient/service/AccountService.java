4bd9eba9728df065564855731b99e9bd93b0059b
Amol <Amol@sowmiyas-MacBook-Pro.local>
2018-02-09 17:02:49 +0000

    Updated class documentation

--- src/main/java/com/amol/spring/mvc/feignclient/service/AccountService.java
+++ src/main/java/com/amol/spring/mvc/feignclient/service/AccountService.java
@@ -9,16 +9,18 @@ import com.amol.spring.mvc.feignclient.vo.AccountVO;
 
 /**
  * @author Amol Waghmare
+ * 
+ * Service interfacee representing the micro service you need to call.
+ *  The underlying framework of the miroservice could be different. 
+ * E.g. The microservice represented by  GET "services/account" is implemented using CXF implementation of Jax-rs.
+ * However this interface uses spring framework annotation for REST. 
+ * Issue?(?): When jax-rs annotations are used, the framework complains about GET method not being defined for the service.
  *
  */
 
 @FeignClient("spring-cloud-account-service")
 public interface AccountService {
-	
-	
 
-//	@RequestMapping("/account", method=Reques)
-//	public AccountVO addAccount(AccountVO accountVO) ;
 	
 	@RequestMapping(path="/services/account", method=RequestMethod.GET)
 	public AccountVO getAccount(@RequestParam("name") String name);
