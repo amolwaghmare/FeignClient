5de447f9efe372363da9f7186f9dc7b578fa882a
Amol <Amol@sowmiyas-MacBook-Pro.local>
2018-02-13 21:40:08 +0000

    Resolved 'No mapping found' error by adding provided dependency 'tomcat-embed-jasper'.

--- src/main/java/com/amol/spring/mvc/feignclient/FeignClientApplication.java
+++ src/main/java/com/amol/spring/mvc/feignclient/FeignClientApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-
+@Configuration
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
@@ -16,4 +21,13 @@ public class FeignClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FeignClientApplication.class, args);
 	}
+	@Bean
+	public ViewResolver getViewResolver(){
+	    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	    resolver.setPrefix("/WEB-INF/jsp/");
+	    resolver.setSuffix(".jsp");
+	    resolver.setViewClass(JstlView.class);
+	    return resolver;
+	}
+	
 }
